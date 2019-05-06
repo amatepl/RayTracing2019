15eaf690d67c775690df2f5b1535b9a32a583cda
Bakaquy <tronphan@ems2.be>
2019-05-06 19:34:00 +0200

    Plot improvements

--- plots.cpp
+++ plots.cpp
@@ -19,10 +19,25 @@ plots::~plots()
 }
 
 void plots::plotPathLoss(double *Data, int TxIndex_i, int TxIndex_j, int rows, int columns, int totalArea){
-    QVector<double> logD(columns-TxIndex_i-1), Prx(columns-TxIndex_i-1);
-    for (int i=0; i<(columns-TxIndex_i-1); ++i){
-        logD[i] = log10(Data[(i+TxIndex_i+1)*rows+TxIndex_j+totalArea*3]);
-        Prx[i] = Data[(i+TxIndex_i+1)*rows+TxIndex_j];
+//    QVector<double> logD(columns-TxIndex_i-1), Prx(columns-TxIndex_i-1);
+//    for (int i=0; i<(columns-TxIndex_i-1); ++i){
+//        logD[i] = log10(Data[(i+TxIndex_i+1)*rows+TxIndex_j+totalArea*3]);
+//        Prx[i] = Data[(i+TxIndex_i+1)*rows+TxIndex_j];
+//    }
+
+    int lengthData = 0;
+    int bias = 0;
+    if(TxIndex_i <= columns/2){
+        lengthData = columns-TxIndex_i-1;
+        bias = TxIndex_i+1;
+    }else{
+        lengthData = TxIndex_i-1;
+    }
+
+    QVector<double> logD(lengthData), Prx(lengthData);
+    for (int i=0; i<(lengthData); ++i){
+        logD[i] = log10(Data[(i+bias)*rows+TxIndex_j+totalArea*3]);
+        Prx[i] = Data[(i+bias)*rows+TxIndex_j];
     }
 
     // create graph and assign data to it:
