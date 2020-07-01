d1ec8e004ad1b2afa2d74871dff3deaf7bbc3b77
Cyprien <cyprienmaes@gmail.com>
2020-07-01 13:08:23 +0200

    New Tab in ApplicationWindow

--- Observer/AppInterface.h
+++ Observer/AppInterface.h
@@ -1,5 +1,6 @@
 #ifndef APPINTERFACE_H
 #define APPINTERFACE_H
+#include <QPoint>
 
 #include "windowobserver.h"
 
@@ -13,6 +14,7 @@ public:
     virtual ~AppInterface(){}
 
     virtual void answer(GraphicsProduct* graphic) = 0;
+    virtual void moveMouse(QPointF mouse) = 0;
 };
 
 #endif //  APPINTERFACE_H
